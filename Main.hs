fc8bd6a7d0442932256f4fcd8f654f1f0a20bcc3
PabloAndresMartinez <pandresymartinez@gmail.com>
2019-05-29 02:16:30 -0300

    Now the program complains if you didn't supply enough qubits.

--- Main.hs
+++ Main.hs
@@ -62,26 +62,28 @@ main = do
     printIt c s = case cfg_outputAs of
       Preview -> preview_withColor c s
       _ -> print_generic cfg_outputAs c s
-    in if cfg_k < 2 || cfg_s < 1 then putStrLn "You must indicate the number of QPUs (k > 1) and their workspace qubit capacity (s > 0); example ./Main -k=2 -s=4" >> putStrLn "" else do
-      putStrLn $ ""
-      putStrLn $ "Original circuit:"
-      printIt input shape
-      putStrLn $ ""
-      putStrLn $ "After preprocessing:"
-      printIt circ shape
-      putStrLn $ ""
-      putStrLn $ "New circuit:"
-      printIt newCircuit shape
-      putStrLn $ ""
-      putStrLn $ "Original gate count: " ++ show gateCountInput
-      putStrLn $ "Original CZ count: " ++ show czsInput
-      putStrLn $ "Original qubit count: " ++ show nWires
-      putStrLn $ ""
-      putStrLn $ "Number of nonlocal CZs: " ++ (show $ countNonLocal segments)
-      putStrLn $ "Number of ebits due nonlocal CZs: " ++ show nEbits
-      putStrLn $ "Number of ebits due to teleportations: " ++ show nTeleports
-      putStrLn $ "Total number of ebits: " ++ show (nEbits+nTeleports)
-      putStrLn $ ""
-      putStrLn $ "Extensions: " ++ if cfg_keepCCZ then "KeepCCZ" else "N/A"
-      putStrLn $ "#QPUs = "++show cfg_k++"; QPU_size = "++show cfg_s
-      putStrLn $ "initSegSize = "++show cfg_initSegSize++"; maxHedgeDist = "++show cfg_maxHedgeDist
+    in if cfg_k < 2 || cfg_s < 1 
+      then putStrLn "You must indicate the number of QPUs (k > 1) and their workspace qubit capacity (s > 0); example ./Main -k=2 -s=4" >> putStrLn "" 
+      else if nQubits < nWires then putStrLn "There are not enough qubits to run the circuit; increase k." >> putStrLn "Z" else do
+        putStrLn $ ""
+        putStrLn $ "Original circuit:"
+        printIt input shape
+        putStrLn $ ""
+        putStrLn $ "After preprocessing:"
+        printIt circ shape
+        putStrLn $ ""
+        putStrLn $ "New circuit:"
+        printIt newCircuit shape
+        putStrLn $ ""
+        putStrLn $ "Original gate count: " ++ show gateCountInput
+        putStrLn $ "Original CZ count: " ++ show czsInput
+        putStrLn $ "Original qubit count: " ++ show nWires
+        putStrLn $ ""
+        putStrLn $ "Number of nonlocal CZs: " ++ (show $ countNonLocal segments)
+        putStrLn $ "Number of ebits due nonlocal CZs: " ++ show nEbits
+        putStrLn $ "Number of ebits due to teleportations: " ++ show nTeleports
+        putStrLn $ "Total number of ebits: " ++ show (nEbits+nTeleports)
+        putStrLn $ ""
+        putStrLn $ "Extensions: " ++ if cfg_keepCCZ then "KeepCCZ" else "N/A"
+        putStrLn $ "#QPUs = "++show cfg_k++"; QPU_size = "++show cfg_s
+        putStrLn $ "initSegSize = "++show cfg_initSegSize++"; maxHedgeDist = "++show cfg_maxHedgeDist
